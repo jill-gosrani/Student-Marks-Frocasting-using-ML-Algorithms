38db521270aaa4aec4837ffd84f693891c2993dc
jill-gosrani <jillgosrani15@gmail.com>
2023-07-05 18:40:37 -0400

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,2 +1,49 @@
 ## Reading of All the data is done here
 ## data split into train-test
+
+import os
+import sys
+
+from src.exceptions import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass 
+
+
+@dataclass
+class DataIngestionConfig:
+    # Whenever performing data ingetions we need some inputs like save data paths(train/test/raw) etc.
+    # Any input we require for data ingestion we will give it to DataIngestionConfig
+    train_data_path: str = os.path.join("artifacts","train.csv")
+    test_data_path: str = os.path.join("artifacts","test.csv")
+    raw_data_path: str = os.path.join("artifacts","data.csv")
+    
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        ##If the code is store in some database it will be read from here
+        logging.info("Entered the data ingestion method")
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+
+            logging.info("train_test_split initiated")
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=35)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of data completed")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
